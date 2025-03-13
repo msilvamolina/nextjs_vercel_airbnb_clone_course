4d1ce7351cffb69dfc4af8cf17201db3749e8851
Martín Silva Molina <msilvamolina@gmail.com>
2025-03-13 03:59:36 +0100

    initial config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import React from "react";
+
+function HomePage() {
   return (
     <div>
-      <h1>Hola!</h1>
+      <h1 className="text-7xl">Home Page</h1>
     </div>
   );
 }
+
+export default HomePage;
