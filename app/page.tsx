1ba49792c95157f9d9ba0be120f9d4df36b977f3
Martín Silva Molina <msilvamolina@gmail.com>
2025-03-13 04:41:09 +0100

    info y about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 function HomePage() {
   return (
     <div>
       <h1 className="text-7xl">Home Page</h1>
+      <Link href="/about" className="text-xl text-blue-500 inline-block mt-8">
+        About page
+      </Link>
     </div>
   );
 }
