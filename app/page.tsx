1048ecb9d9e9f76e4f5ed0aaa6257ac3f2196556
Martín Silva Molina <msilvamolina@gmail.com>
2025-03-13 11:25:28 +0100

    pruebas con el nuevo editor

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ function HomePage() {
   return (
     <div>
       <h1 className="text-7xl">Home Page</h1>
-      <Link href="/about" className="text-xl text-blue-500 inline-block mt-8">
+      <Link
+        href="/about"
+        className="text-xl text-primary-500 inline-block mt-8 py-6 px-12 border border-primary-500 rounded-full hover:bg-black hover:text-white"
+      >
         About page
       </Link>
     </div>
