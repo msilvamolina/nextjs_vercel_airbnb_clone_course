316189cf1d9ffdb17a662e8f8dacc0049b360d83
Martín Silva Molina <msilvamolina@gmail.com>
2025-03-19 05:45:30 +0100

    feat: add button component to home page
    Introduce a new button component from the UI library to the home page for better user interaction. The button is styled with default variant, large size, and capitalized text.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 function HomePage() {
   return (
     <div>
       <h1 className="text-7xl">Home Page</h1>
+      <Button variant="default" size="lg" className="capitalize m-8">
+        click me
+      </Button>
     </div>
   );
 }
