2314d2fe4a9754aaa663c756b3908d4ef65b0cc1
Martín Silva Molina <msilvamolina@gmail.com>
2025-03-19 05:24:08 +0100

    start course

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import Link from "next/link";
 import React from "react";
 
 function HomePage() {
   return (
     <div>
       <h1 className="text-7xl">Home Page</h1>
-      <Link
-        href="/about"
-        className="text-xl text-primary-500 inline-block mt-8 py-6 px-12 border border-primary-500 rounded-full hover:bg-black hover:text-white"
-      >
-        About page
-      </Link>
     </div>
   );
 }
